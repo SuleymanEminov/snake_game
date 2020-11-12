9246d8a47651611acff8b9a001a7a7ffa3aa9fa1
Suleyman Eminov <58894499+SuleymanEminov@users.noreply.github.com>
2020-11-12 00:00:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
-from snake import Snake
 import time
+from turtle import Screen
+
+from snake import Snake
 
 # set up screen
 screen = Screen()
@@ -9,29 +10,27 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snake = Snake()
 
-screen.listen()
+def main():
+    # create snake
+    snake = Snake()
 
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+    screen.listen()
 
+    # listen to keys. Turn snake
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(.05)
 
-game_is_on = True
-while game_is_on:
-    screen.update(  )
-    time.sleep(.05)
-
-    snake.move()
-
-
-
-
+        snake.move()
 
 
-
+main()
 
 screen.exitonclick()
